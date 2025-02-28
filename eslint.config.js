91920b6e58f1320bb742dbddc0128ab1b0aacc89
SONO RICK <r.patetta.2724@vallauri.edu>
2025-02-28 21:11:37 +0100

    Production Fixed

---
+++ eslint.config.js
@@ -0,0 +1,61 @@
+import ts from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
+
+export default [
+  {
+    ignores: [
+        "**/*.log",
+        "**/.DS_Store",
+        ".vscode/settings.json",
+        "**/.history",
+        "**/.yarn",
+        "**/bazel-*",
+        "**/bazel-bin",
+        "**/bazel-out",
+        "**/bazel-qwik",
+        "**/bazel-testlogs",
+        "**/dist",
+        "**/dist-dev",
+        "**/lib",
+        "**/lib-types",
+        "**/etc",
+        "**/external",
+        "**/node_modules",
+        "**/temp",
+        "**/tsc-out",
+        "**/tsdoc-metadata.json",
+        "**/target",
+        "**/output",
+        "**/rollup.config.js",
+        "**/build",
+        "**/.cache",
+        "**/.vscode",
+        "**/.rollup.cache",
+        "**/dist",
+        "**/tsconfig.tsbuildinfo",
+        "**/vite.config.ts",
+        "**/*.spec.tsx",
+        "**/*.spec.ts",
+        "**/.netlify",
+        "**/pnpm-lock.yaml",
+        "**/package-lock.json",
+        "**/yarn.lock",
+        "**/server",
+    ],
+},
+{
+  files:["src/**/*.ts*"]
+},
+  {
+    languageOptions: {
+      parser: tsParser,
+      sourceType: "module",
+    },
+    plugins: {
+      "@typescript-eslint": ts,
+    },
+    rules: {
+      "@typescript-eslint/no-unused-vars": "warn",
+    },
+  },
+];
