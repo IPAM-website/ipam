15c608859ec93e28b7629e8ae6cad3857fa56e3a
Lorenzo <bellanlorenzo06@gmail.com>
2025-02-28 20:56:53 +0100

    Inserimento file di configurazione docker e db

---
+++ db.ts
@@ -0,0 +1,17 @@
+import postgres from 'postgres'
+
+const { POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_DB, POSTGRES_HOST, POSTGRES_PORT } = process.env
+
+if (!POSTGRES_USER || !POSTGRES_PASSWORD || !POSTGRES_DB || !POSTGRES_HOST) {
+    throw new Error('Missing required environment variables')
+}
+
+const sql = postgres({ 
+    hostname: POSTGRES_HOST,
+    database: POSTGRES_DB,
+    username: POSTGRES_USER,
+    password: POSTGRES_PASSWORD,
+    port: parseInt(POSTGRES_PORT || '5432')
+});
+
+export default sql;
