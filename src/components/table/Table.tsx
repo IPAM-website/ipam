5e17938fee8c164575de6313a021efa17d7ca53e
Lorenzo <bellanlorenzo06@gmail.com>
2025-03-21 10:29:02 +0100

    Quasi fine tabella dinamica

---
+++ src/components/table/Table.tsx
@@ -0,0 +1,17 @@
+import { $, component$, useTask$, useSignal } from "@builder.io/qwik";
+import TitoloTable from "~/components/table/TitoloTable";
+import Dati from "~/components/table/Dati_Headers";
+import ButtonAdd from "~/components/table/ButtonAdd";
+
+interface TableProps { title?: string, dati ?: any, nomeTabella: string, nomePulsante : string }
+
+export default component$<TableProps>(({ title, dati, nomeTabella, nomePulsante }) => {
+
+    return (
+        <div class="bg-white rounded-lg shadow-[0px_4px_12px_0px_rgba(0,0,0,0.04)] border-1 border-offset-[-1px] border-neutral-200 mt-12">
+            <TitoloTable nomeTitolo={title}></TitoloTable>
+            <Dati dati={dati} nomeTabella={nomeTabella}></Dati>
+            <ButtonAdd nomePulsante={nomePulsante}></ButtonAdd>
+        </div>
+    )
+})
